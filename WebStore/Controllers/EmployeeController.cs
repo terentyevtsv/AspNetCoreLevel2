6370668bb8dd6752f485e0e2094bf12e436b37ea
terentyevtsv <terentyevtsv@ya.ru>
2019-09-18 21:19:21 +0400

    Добавил методы изменить, удалить, добавить сотрудников.

--- WebStore/Controllers/EmployeeController.cs
+++ WebStore/Controllers/EmployeeController.cs
@@ -11,12 +11,8 @@ namespace WebStore.Controllers
     /// </summary>
     public class EmployeeController : Controller
     {
-        private readonly List<EmployeeView> _employeeViews;
-
-        public EmployeeController()
-        {
-            // Заготовленный список сотрудников
-            _employeeViews = new List<EmployeeView>
+        private static readonly List<EmployeeView> _employeeViews
+            = new List<EmployeeView>
             {
                 new EmployeeView
                 {
@@ -39,7 +35,6 @@ namespace WebStore.Controllers
                     HireDate = new DateTime(2009, 6, 1)
                 }
             };
-        }
 
         /// <summary>
         /// Действие выводит весь список сотрудников
@@ -64,6 +59,72 @@ namespace WebStore.Controllers
                 return NotFound();
 
             return View(employeeView);
-        }        
+        }
+
+        /// <summary>
+        /// Действие возвращает пустую форму нового сотрудника если не передается id
+        /// Иначе ищет сотрудника и возвращает представление с формой по сотруднику для изменения
+        /// </summary>
+        /// <param name="id">id сотрудника</param>
+        /// <returns></returns>
+        public IActionResult Edit(int? id)
+        {
+            var employeeView = id.HasValue
+                ? _employeeViews
+                    .SingleOrDefault(emp => emp.Id == id.Value)
+                : new EmployeeView();
+
+            return View(employeeView);
+        }
+
+        /// <summary>
+        /// Передает заполненные данные по сотруднику в форме
+        /// </summary>
+        /// <param name="employeeView">Модель сотрудника</param>
+        /// <returns>Возвращает обновленный список сотрудников/returns>
+        [HttpPost]
+        public IActionResult Edit(EmployeeView employeeView)
+        {
+            if (employeeView.Id == 0)
+            {
+                var id = _employeeViews.Any()
+                    ? _employeeViews.Max(emp => emp.Id) + 1
+                    : 1;
+                employeeView.Id = id;
+
+                _employeeViews.Add(employeeView);
+
+                return RedirectToAction("Index");
+            }
+
+            var tmpEmployeeView = _employeeViews
+                .SingleOrDefault(emp => emp.Id == employeeView.Id);
+            if (ReferenceEquals(tmpEmployeeView, null))
+                return NotFound();
+
+            tmpEmployeeView.Age = employeeView.Age;
+            tmpEmployeeView.BirthDate = employeeView.BirthDate;
+            tmpEmployeeView.FirstName = employeeView.FirstName;
+            tmpEmployeeView.HireDate = employeeView.HireDate;
+            tmpEmployeeView.Patronymic = employeeView.Patronymic;
+            tmpEmployeeView.SurName = employeeView.SurName;
+
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// Удаляет сотрудника
+        /// </summary>
+        /// <param name="id">id сотрудника</param>
+        /// <returns>Возвращет</returns>
+        public IActionResult Delete(int id)
+        {
+            var employeeView = _employeeViews
+                .SingleOrDefault(emp => emp.Id == id);
+            if (!ReferenceEquals(employeeView, null))
+                _employeeViews.Remove(employeeView);
+
+            return RedirectToAction("Index");
+        }
     }
 }
