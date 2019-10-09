f5e185b7b23e84e3c639226ed38dc59eb820435a
terentyevtsv <terentyevtsv@ya.ru>
2019-10-09 23:28:40 +0400

    Добавил подытог и налоги.

--- WebStore/ViewModels/CartViewModel.cs
+++ WebStore/ViewModels/CartViewModel.cs
@@ -11,5 +11,40 @@ namespace WebStore.ViewModels
         {
             get { return Items?.Sum(item => item.Value) ?? 0; }
         }
+
+        private decimal GetPrice()
+        {
+            var price = Items
+                            ?.Sum(item => item.Value * item.Key.Price)
+                        ?? 0;
+            return price;
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                var price = GetPrice();
+                return price;
+            }
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                var price = GetPrice();
+                return (decimal)1.13 * price;
+            }
+        }
+
+        public decimal Tax
+        {
+            get
+            {
+                var price = GetPrice();
+                return price * (decimal) 0.13;
+            }
+        }
     }
 }
