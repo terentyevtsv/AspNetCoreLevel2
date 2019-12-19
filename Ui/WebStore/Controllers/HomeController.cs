2fcc8b9191ea2cbdd1e49e251e2b43eed4f3a818
Terentiev_SV <TerentievSV@samnipineft.ru>
2019-12-19 08:38:18 +0400

    Built-in logging

--- Ui/WebStore/Controllers/HomeController.cs
+++ Ui/WebStore/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using WebStore.Interfaces;
 
 namespace WebStore.Controllers
@@ -7,14 +8,24 @@ namespace WebStore.Controllers
     public class HomeController : Controller
     {
         private readonly IValueService _valueService;
+        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(IValueService valueService)
+        public HomeController(IValueService valueService, ILogger<HomeController> logger)
         {
             _valueService = valueService;
+            _logger = logger;
         }
 
         public async Task<IActionResult> Index()
         {
+            _logger.LogInformation("index action requested");
+            _logger.LogTrace("trace! winter is coming!");
+            _logger.LogInformation("info! winter is coming!");
+            _logger.LogWarning("warning! winter is coming!");
+            _logger.LogDebug("debug! winter is coming!");
+            _logger.LogError("error! winter is coming!");
+            _logger.LogCritical("critical! winter is coming!");
+
             var values = await _valueService.GetAsync();
             return View(values);
         }
